f3c46586213274ff10bd6c0ad5619d87e81d250f
Miss-Coder-developer <misscoderexpert@gmail.com>
2021-06-23 19:09:37 +0400

    change func

--- src/components/todoItem/todoItem.js
+++ src/components/todoItem/todoItem.js
@@ -11,9 +11,14 @@ import './style.css'
 export default function TodoItem(props) {
     let [isEditing, setIsEditing] = useState(false)
     console.log(isEditing)
+    let styles = {
+        row: {
+            backgroundColor: 'rgb(35 118 31 / 50%)'
+        }
+    }
     let handleEdit = (e) => {
         setIsEditing(!isEditing)
-        console.log(document.querySelector(`.editButton`).innerHTML);
+        // console.log(document.querySelector(`.editButton`).innerHTML);
         if (isEditing == true) {
             let newval = document.querySelector(`.inputitle`).value
             props.data.title = newval;
@@ -29,6 +34,11 @@ export default function TodoItem(props) {
         }
     }
 
+    const aaa = (e) => { 
+        props.isComplete(props.data.id);
+    
+    }
+
     return (
         <>
 
@@ -37,29 +47,30 @@ export default function TodoItem(props) {
                 {!isEditing ? <div className='title'>{props.data.isComplete ? <del>{props.data.title}</del> : props.data.title}</div> :
                     <div className='title' ><input type='text' style={{ border: 'none' }} className='title inputitle' defaultValue={props.data.title} /></div>
                 } */}
-            <TableRow hover key={props.index}  >
+            <TableRow hover key={props.index} style={props.data.isComplete ? styles.row : null} onClick={()=>{props.isComplete(props.data.id)}}>
                 <TableCell align="left" >
                     <Checkbox
                         color="primary"
                         inputProps={{ 'aria-label': 'secondary checkbox' }}
-                        onChange={() => { props.isComplete(props.data.id) }}
+                        onChange={aaa}
+                        checked={props.data.isComplete ? 'checked' : false}
                     />
                     {/* <div className='checkbox'><input type="checkbox" onChange={() => { props.isComplete(props.data.id) }} /></div> */}
                     {/* {props.data.id} */}
                 </TableCell>
-                <TableCell component="th" scope="row" style={props.data.isComplete ? {opacity: '0.2'} : null}>
+                <TableCell component="th" scope="row" >
                     {!isEditing ? <div className='title'>{props.data.title}</div> :
                         <div className='title' ><input type='text' style={{ border: 'none' }} className='title inputitle' defaultValue={props.data.title} /></div>
                     }
                 </TableCell>
-                <TableCell align="left" style={props.data.isComplete ? {opacity: '0.2'} : null}>
+                <TableCell align="left" >
                     <Button className='editButton' disabled={props.data.isComplete ? 'disabled' : false} startIcon={isEditing ? <SaveIcon /> : <EditIcon />} onClick={() => { handleEdit(props.data.id) }} variant="contained" color="primary">{isEditing ? `Save` : `Edit`}</Button>
                 </TableCell>
-                <TableCell align="left" style={props.data.isComplete ? {opacity: '0.2'} : null}>
+                <TableCell align="left" >
                     <Button className='removebutton' disabled={props.data.isComplete ? 'disabled' : false} startIcon={<DeleteIcon />} onClick={() => { props.removeItem(props.index) }} variant="contained" color="secondary">Delete</Button>
 
                 </TableCell>
-                <TableCell align="left" style={props.data.isComplete ? {opacity: '0.2'} : null}>
+                <TableCell align="left" >
                     <Button className='archivebutton' disabled={props.data.isComplete ? 'disabled' : false} startIcon={<ArchiveIcon />} onClick={() => { props.archiveitem(props.index) }} variant="contained" color="default">Archive</Button>
                 </TableCell>
             </TableRow>
