153728a23237ebd172c2aa83b8c43ea1ec7016ab
Miss-Coder-developer <misscoderexpert@gmail.com>
2021-06-28 15:29:09 +0400

    styles

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,9 @@
 import { NavLink } from "react-router-dom"
-
+import './style.css'
 export default function Header(){
     return(
         <>
-        <div>
+        <div className="header">
             <NavLink to={`/`}> Home</NavLink>
             <NavLink to={`/about`}> About </NavLink>
             <NavLink to={`/contact`}> Contact</NavLink>
